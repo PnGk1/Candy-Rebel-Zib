aa1b1ebc1190117b59522b1d0698054d4c01c7e2
PeteGk123 <triplerocketgr@gmail.com>
2025-09-17 19:06:34 +0300

    fix

--- Candy Rebel Zib/Assets/InputSystem/PlayerInputManager.cs
+++ Candy Rebel Zib/Assets/InputSystem/PlayerInputManager.cs
@@ -5,7 +5,7 @@ namespace Baseplate.InputManager
     public class PlayerInputManager : MonoBehaviour
     {
         //Input System
-        private PlayerControls playerControls;
+        public PlayerControls playerControls;
         private void Awake()
         {
             playerControls = new PlayerControls();
